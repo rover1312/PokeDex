21b74dd0ae918b782a8d4095dd8853ee8bd40bab
rover1312 <rishavh1312@gmail.com>
2021-07-24 16:58:34 +0530

    PokeDex-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
